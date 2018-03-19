91d332ab56433d086d904e387f805032872fce8c
David Gofman <dgofman@equinix.com>
2018-03-19 12:02:12 -0700

    update files

--- resources/create_exe.cmd
+++ resources/create_exe.cmd
@@ -1,5 +1,5 @@
 SET LAUNCH4J=C:\Program Files (x86)\Launch4j
 
 CD ../client
-CALL mvn clean assembly:assembly
+CALL mvn -Dmaven.test.skip=true clean assembly:assembly
 "%LAUNCH4J%/launch4jc.exe" ../resources/Amphibia.xml
